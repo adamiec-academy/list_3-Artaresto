d234aad01fab24b5b5c2acc56d98bb8431a588c3
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-11-02 11:56:27 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,2 +1,20 @@
+def is_perfect(n):
+    a = 0
+    if n < 6:
+        return False
+    for i in range(1, n):
+        if (n % i) == 0:
+            a = a + i
+    if a == n:
+        return True
+
 def get_perfect_numbers(n):
-    return []
+    result = []
+    a = n
+    while len(result) != n: 
+        for _ in range(a+1):
+            if is_perfect(a) == True and a not in result:
+                result.append(a)
+            else:
+                a += 1
+    return result
