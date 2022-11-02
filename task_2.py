a922c3c9e8446aac4eb0471b89b6995cf1325177
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-11-02 13:09:35 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,1 +1,12 @@
-
+def fibonacci_sequence(n):
+    result = []
+    while len(result) != n:
+            if n == 1: 
+                result.append(0)
+            elif n == 2 or len(result) == 0:
+                result.append(0)
+                result.append(1)
+                result.append(1)
+            else:
+                    result.append(result[-1] + result[-2])
+    return result
