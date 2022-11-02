19412f550975c9c7b39d726cd7f3bad17b46ed36
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-11-02 13:14:54 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -6,7 +6,8 @@ def fibonacci_sequence(n):
             elif n == 2 or len(result) == 0:
                 result.append(0)
                 result.append(1)
-                result.append(1)
+                if n > 2:
+                    result.append(1)
             else:
                 result.append(result[-1] + result[-2])
     return result
